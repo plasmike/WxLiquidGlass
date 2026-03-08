05e4ad4c1a7c1777a99763aed7c39eb3b1c178cc
mike <262139264+plasmike@users.noreply.github.com>
2026-03-08 12:40:31 +0100

    add WxLiquidGlass.mm header

---
+++ src/WxLiquidGlassCommon.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#if defined(__APPLE__) && defined(__MACH__)
+  #define PLATFORM_OSX
+#endif
+
+#ifdef PLATFORM_OSX
+  // Dont include AppKit/Foundation bc this header is included by WxLiquidGlass.cpp which is a standard C++ file
+
+  // Forward declarations for internal functions used by backend
+  extern "C" {
+    int AddGlassEffectView(void* nativeViewPTr, bool opaque);
+
+    void RemoveGlassEffectView(int viewID);
+  }
+
+#endif
