ab931e16e0aabeaf4780ed96e4c9eeb1e27ea714
mike <262139264+plasmike@users.noreply.github.com>
2026-03-08 13:04:39 +0100

    empty window for testing

---
+++ examples/LiquidGlassDemo/EmptyWindow.cpp
@@ -0,0 +1,15 @@
+#include "EmptyWindow.h"
+
+
+MyFrame::MyFrame()
+    : wxFrame(nullptr, wxID_ANY, "Wx Liquid Glass", wxDefaultPosition, wxSize(500, 250))
+{
+
+
+}
+
+MyFrame::~MyFrame()
+{
+
+
+}
