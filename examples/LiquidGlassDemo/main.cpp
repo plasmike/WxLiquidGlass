82887fb105749f360f38f21a04c5cdc70092e00e
mike <262139264+plasmike@users.noreply.github.com>
2026-03-08 13:03:32 +0100

    example for testing

---
+++ examples/LiquidGlassDemo/main.cpp
@@ -0,0 +1,16 @@
+#include <wx/wx.h>
+#include "EmptyWindow.h"
+
+
+class MyApp : public wxApp
+{
+public:
+    bool OnInit() override
+    {
+        MyFrame* frame = new MyFrame();
+        frame->Show(true);
+        return true;
+    }
+};
+
+wxIMPLEMENT_APP(MyApp);
