3769cf01220926844579ead20016c8bef1564554
mike <262139264+plasmike@users.noreply.github.com>
2026-03-09 18:47:53 +0100

    added liquid glass comparison that uses swift

---
+++ examples/LiquidGlassDemo/SwiftComparison.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+import AppKit
+
+@main
+struct TransparentWindowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .background(Color.clear)
+                .onAppear {
+                    makeWindowTransparent()
+                }
+        }
+    }
+
+    func makeWindowTransparent() {
+        if let window = NSApplication.shared.windows.first {
+            window.title = "Liquid Glass using SwiftUI and AppKit"
+            window.isOpaque = false
+            window.backgroundColor = .clear
+            window.styleMask.insert(.titled)
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+          // Liquid glass rectangle
+          RoundedRectangle(cornerRadius: 16)
+              .glassEffect(.regular, in: .rect(cornerRadius: 16)) // either .regular or .clear
+              .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+        }
+}
