c26025a667cc1567b258b7460693151f8cb6661a
mike <262139264+plasmike@users.noreply.github.com>
2026-03-18 20:06:07 +0100

    Switch from conanfile.txt to conanfile.py to allow for more control

---
+++ conanfile.py
@@ -0,0 +1,24 @@
+import os
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, cmake_layout
+
+class WxLiquidGlassRecipe(ConanFile):
+  name = "WxLiquidGlass"
+
+  # config
+  settings = "os", "compiler", "build_type", "arch"
+  options = {"shared": [True, False], "fPIC": [True, False]}
+  default_options = {"shared": False, "fPIC": True}
+
+  def requirements(self):
+    self.requires("wxwidgets/3.3.1")
+
+  def layout(self):
+    cmake_layout(self)
+
+  def generate(self):
+    tc = CMakeToolchain(self)
+    tc.user_presets_path = None
+    tc.generate()
+    cmake = CMakeDeps(self)
+    cmake.generate()
